68022fbfbb4424da4ff4d6c7fd5fb3098fbb0d56
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-12-25 19:39:30 -0500

    Add function to access for the name

--- App.tsx
+++ App.tsx
@@ -1,24 +1,11 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Text } from "react-native";
 
-import useCachedResources from "./hooks/useCachedResources";
-import useColorScheme from "./hooks/useColorScheme";
-import Navigation from "./navigation";
+const getFullName = (firstName, secondName, thirdName) => {
+  return firstName + " " + secondName + " " + thirdName;
+};
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
-}
+const Cat = () => {
+  return <Text>Hello, I am a {getFullName("Rum", "Tum", "Tugger")}!</Text>;
+};
+export default Cat;
