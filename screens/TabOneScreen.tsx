de3410a78f679a30fb84e3fd39ae584a92d28d3d
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-12-25 19:16:49 -0500

    Finished first portion

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -12,6 +12,16 @@ const App = () => {
           style={{ width: 200, height: 200 }}
         />
       </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
+        defaultValue="You can type in me"
+      />
     </ScrollView>
   );
 };
+
+export default App;
