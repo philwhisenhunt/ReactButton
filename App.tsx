73ba1c688c0d7bdae9fd40e2b44405a894d5b4ee
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-12-29 18:27:00 -0500

    Connecting old version from example template to load the other tabs

--- App.tsx
+++ App.tsx
@@ -1,25 +1,6 @@
-import React, { useState } from "react";
-import { Text, TextInput, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-const PizzaTranslator = () => {
-  const [text, setText] = useState("");
-  return (
-    <view style={{ padding: 20 }}>
-      <Text style={{ padding: 10, fontSize: 42 }}>Turn a noun into a verb</Text>
-
-      <TextInput
-        style={{ height: 50 }}
-        placeholder="Type here to translate"
-        onChangeText={(text) => setText(text)}
-        defaultValue={text}
-      />
-      <Text style={{ padding: 10, fontSize: 42 }}>
-        {text
-          .split(" ")
-          .map((word) => word && word + "-ify")
-          .join(" ")}
-      </Text>
-    </view>
-  );
-};
-export default PizzaTranslator;
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
