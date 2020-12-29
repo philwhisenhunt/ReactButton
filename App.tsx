3fe18491ca859efff8f30316d0bc873c03d5abe1
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-12-29 18:09:39 -0500

    Add explainer text

--- App.tsx
+++ App.tsx
@@ -5,8 +5,10 @@ const PizzaTranslator = () => {
   const [text, setText] = useState("");
   return (
     <view style={{ padding: 20 }}>
+      <Text style={{ padding: 10, fontSize: 42 }}>Turn a noun into a verb</Text>
+
       <TextInput
-        style={{ height: 100 }}
+        style={{ height: 50 }}
         placeholder="Type here to translate"
         onChangeText={(text) => setText(text)}
         defaultValue={text}
