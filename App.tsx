661e855e3e9b204c6f8e30b4b5dd4112b76f3b7e
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-12-28 22:30:53 -0500

    Adding to text input part of lesson

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
-import { Text, TextInput, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, TextInput, View } from "react-native";
 
 const PizzaTranslator = () => {
-  
-}
+  const [text, setText] = useState("");
+  return <view style={{ padding: 10 }}></view>;
+};
